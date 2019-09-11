c11c5f625b9b873c340845ce979c2b7263a73461
Kishor Naik <kishor.naik011.net@gmail.com>
2019-09-11 15:11:49 +0530

    Code Commit

---
+++ Sol_Multiple_Button/Sol_Multiple_Button/Controllers/DemoController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Sol_Multiple_Button.Controllers
+{
+    public class DemoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult OnSubmit()
+        {
+            return View("Index");
+        }
+
+
+        [HttpPost]
+        public IActionResult OnCancel()
+        {
+            return View("Index");
+        }
+    }
+}
